561535b9bd7aa2fae31172966ae5f77f0b21ae31
Anya Lozovaya <anyalozovaya2014@gmail.com>
2020-09-29 16:34:37 +0400

    feat: add functions

--- src/index.js
+++ src/index.js
@@ -1,12 +1,50 @@
-
-exports.min = function min (array) {
-  return 0;
+exports.min = function min(array) {
+    if (array === undefined || array.length === 0) {
+        return 0
+    }
+    if (array.length > 0) {
+        let arrayOfValue = array
+        let minValue = arrayOfValue[0]
+        for (let i = 1; i < arrayOfValue.length; i++) {
+            if (minValue > arrayOfValue[i]) {
+                minValue = arrayOfValue[i]
+            }
+        }
+        return minValue
+    }
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max(array) {
+    if (array === undefined || array.length === 0) {
+        return 0
+    }
+    if (array.length > 0) {
+
+        let arrayNew = array
+        if (arrayNew.length === 0) return 0
+        let maxValue = arrayNew[0]
+        for (let i = 1; i < arrayNew.length; i++) {
+            if (maxValue < arrayNew[i]) {
+                maxValue = arrayNew[i]
+            }
+        }
+        return maxValue
+    }
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg(array) {
+    if (array === undefined || array.length === 0) {
+        return 0
+    }
+    if (array.length > 1) {
+        let arrayOfValue = array
+        let avg = 0
+        let sum = 0
+        let lengtharrayOfValue = arrayOfValue.length
+        for (let i = 0; i < lengtharrayOfValue; i++) {
+            sum += arrayOfValue[i]
+        }
+        avg = sum / lengtharrayOfValue
+        return avg
+    }
 }
